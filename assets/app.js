16869feb3c197dcf8fd7204b30b1b82e2b572a17
Nanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2024-08-08 01:27:40 -0500

    revert useragent experiment

--- assets/app.js
+++ assets/app.js
@@ -15,7 +15,6 @@ const API = {
             method,
             headers: {
                 "Authorization": token
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.3"
             }
         });
     }
